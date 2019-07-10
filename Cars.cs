f64ce049bb365fc5bee5f6e10b4c3fbee2c9546c
dek-haji <deeeqhaji@gmail.com>
2019-07-10 13:51:47 -0500

    i did List, Dictionary and class understanding code

---
+++ Cars.cs
@@ -0,0 +1,19 @@
+
+namespace CarLot
+{
+    public class Car
+    {
+        public int _miles;
+        public int Year { get; set; }
+        public string Model { get; set; }
+        public string Make { get; set; }
+        public int Price { get; set; }
+
+        // public car()
+        // {}
+        //     public car(int startingMile)
+        //     {
+        //         _miles = startingMile;
+        // }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CarLot
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+                var students = new List<string>()
+                {
+                    "dek", "haji", "xashi",
+                    "ahmed", "axmed", "canab"
+                };
+               // students.ForEach(singleStudent => Console.WriteLine($"student names is : {singleStudent}"));
+                students.Add("cali");
+               // students.ForEach(singleStudent => Console.WriteLine($"after i added cali : {singleStudent}"));
+
+                var teachers = new List<string> ()
+                {
+                    "adam", "heather", "Jissie"
+                };
+               // teachers.ForEach(oneTeacher => Console.WriteLine($"teachers  : {oneTeacher}"));
+                teachers.Add("ahmed");
+                //teachers.ForEach(oneTeacher => Console.WriteLine($"teachers  : {oneTeacher}"));
+                teachers.Remove("ahmed");
+                // teachers.ForEach(oneTeacher => Console.WriteLine($"after the remove  : {oneTeacher}"));
+
+                teachers.AddRange(students);
+                //teachers.ForEach(oneTeacher => Console.WriteLine($"after adding another list  : {oneTeacher}"));
+
+                string myName = teachers.Find(student => student.StartsWith("d"));
+                //Console.WriteLine($"yoow i found my {myName}");
+
+                var shortNames = teachers.Find(teacher => teacher.Length == 3);
+                //Console.WriteLine(shortNames);
+
+            var mercedes = new Dictionary<string, string>()
+            {
+                {"Year", "1914"},
+                {"Make", "mercedes"},
+                {"Model", "Benz"},
+                {"Price", "30000"}
+            };
+            Dictionary<string, string> tesla = new Dictionary<string, string>()
+            {
+                {"Year", "2017"},
+                {"Make", "Tesla"},
+                {"Model", "X"},
+                {"Price", "80000.45"}
+            };
+            Console.WriteLine(mercedes["Year"]);
+
+
+
+            //with class
+            var toyota = new Car();
+            toyota.Year = 2018;
+            toyota.Make = "Toyota";
+            toyota.Model = "Camry";
+            toyota.Price = 25440;
+
+            //var Garage = List<Dictionary< car>>();
+//use dot notation to console log the class.
+             Console.WriteLine(toyota.Model);
+
+
+        }
+
+        // private static object List<T>()
+        // {
+        //     throw new NotImplementedException();
+        // }
+    }
+}
